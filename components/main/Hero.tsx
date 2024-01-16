3e1bbb920905212f60cb52d094561c67dbb12ecc
0xdeeznode <nalanarud@gmail.com>
2024-01-16 04:29:20 -0500

    add Shapes component

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -1,19 +1,12 @@
 import React from 'react'
 import HeroContent from '../sub/HeroContent'
+import Shapes from '../sub/Shapes'
 
 const Hero = () => {
   return (
-    <div className='relative flex flex-col h-full w-full'>
-{/*     <video 
-        autoPlay
-        muted
-        loop
-        className='rotate-180 absolute top-[-340px] left-0 z-[1] h-full w-full object-cover'
-        >
-          <source src='/blackhole.webm' type='video/webm' />
-        </video> 
-*/}
+    <div className='relative flex flex-col md:flex-row h-full w-full'>
         <HeroContent />
+        <Shapes />
     </div>
   )
 }
